59a3c905177654cafa3537ed2cb258a88519e13e
程翠翠(북경개발센터 정보시스템실) <Cheng.CuiCui@home.e-kmall.com>
2016-05-16 10:31:44 +0800

    all

---
+++ Mvc3First/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc3First.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            //return View("About");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult JiaFa(int valueOne,int valueTwo) {
+            int retValue = valueOne + valueTwo;
+            return Json(retValue, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Redirect() {
+
+            return this.Redirect("About");
+        }
+    }
+}
